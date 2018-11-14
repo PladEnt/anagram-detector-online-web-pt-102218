5680a66bcd9c300ee0843f73dbcd18654d1cb1fc
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-11-14 14:20:38 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,16 @@
 # Your code goes here!
 class Anagram
-  attr_accessor 
+  attr_accessor :name
   
   def initialize(word)
-    
+    @name = word
   end
   
   def match(words)
     words.each do |word|
-      
+      word.split("").sort
+      @name>split("").sort
+      if word == @name
+        
   end
 end
